56439f73de596ac8a899b7c1d4a84bdb4f519952
ymaeda <35252676+yuukimaeda@users.noreply.github.com>
2018-01-12 17:55:44 +0900

    modify

--- phpinfo.php
+++ phpinfo.php
@@ -1,4 +1,19 @@
-		<?php
-			$host-name = gethostname();
-			echo "<p>Host-Name : $host-name</p><br>";
-		?>
+<!DOCTYPE html>
+<html lang="ja">
+        <head>
+                <meta charset="UTF-8">
+                <title>hello</title>
+        </head>
+        <body>
+                <p>PHP-Page</p>
+
+                <?php
+                        $host = gethostname();
+                        echo "<p>Host-Name : $host</p><br>";
+                ?>
+
+                <a href="http://www.jtpedu-lab.com">To:index(S3)</a><br>
+                <a href="http://apptest.jtpedu-lab.com">To:Application-Server(EC2)</a><br>
+                <a href="./phpinfo.php">To:PHP-Page</a><br>
+        </body>
+</html>
