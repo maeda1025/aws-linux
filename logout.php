0823bc02377d2ea8191ce40f99fb689f77a7e0a9
ymaeda <35252676+yuukimaeda@users.noreply.github.com>
2018-01-19 16:40:31 +0900

    add

---
+++ logout.php
@@ -0,0 +1,33 @@
+﻿<?php
+    session_start();
+?>
+
+<html>
+<head><title>LogOut</title></head>
+<body>
+
+<?php
+    print('セッション変数の一覧を表示します。<br>');
+    print_r($_SESSION);
+    print('<br>');
+
+    print('セッションIDを表示します。<br>');
+    print($_COOKIE["PHPSESSID"].'<br>');
+
+    print('<p>ログアウトします</p>');
+
+    $_SESSION = array();
+
+    if (isset($_COOKIE["PHPSESSID"])) {
+        setcookie("PHPSESSID", '', time() - 1800, '/');
+    }
+
+    session_destroy();
+?>
+
+<p>
+<a href="./login.php">ログイン画面へ戻る</a>
+</p>
+
+</body>
+</html>
