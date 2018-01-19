59707de44b6f6bf2ffde2471050f736130e4fd7e
ymaeda <35252676+yuukimaeda@users.noreply.github.com>
2018-01-19 16:32:15 +0900

    modify

--- test.php
+++ test.php
@@ -15,6 +15,15 @@
         print('セッションIDは '.$_COOKIE["PHPSESSID"].' です。');
     }
 
+
+    if (!isset($_COOKIE[session_name()])){
+        print('初回の訪問です。セッションを開始します。');
+    }else{
+        print('セッションは開始しています。<br>');
+        print('クッキーは '.$_COOKIE[session_name()].' です。<br>');
+        print('session_id()は '.session_id().' です。<br>');
+    }
+
 ?>
 
 </body>
