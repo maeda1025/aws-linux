c84f81a2d60cfb0638017aa1b43edfbab18fbf7b
ymaeda <35252676+yuukimaeda@users.noreply.github.com>
2018-01-25 17:43:28 +0900

    modify

--- user_add_delete_db.php
+++ user_add_delete_db.php
@@ -14,32 +14,24 @@ else {
 }
 //-----------------------------------------------------------
 
-
-print $userid;
-print $username;
-print $delete_username;
-
 connect_db($_SERVER['DBHOST'],$_SERVER['DBUSER'],$_SERVER['DBPASS'],$_SERVER['DBNAME']);
 
 if(!$username == ""){
 	insert_data('users','userid',$userid,'username',$username);
-print "if";
 }
 
 else if(!$delete_username == ""){
 	delete_data('users','username',$delete_username);
-print "else if";
 }
 
 else{
 	echo "error";
 	exit(1);
 }
-//disconnect_db($link);
   $close_flag = mysql_close($link);
 //----------------------------------------------------------
 
 
-// header("Location:./userlist.php",true,303);
+ header("Location:./userlist.php",true,303);
 
 ?>
