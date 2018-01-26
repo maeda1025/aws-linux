5cbdbdb686204ccbcad25c389ff7eceb6b2f0925
ymaeda <35252676+yuukimaeda@users.noreply.github.com>
2018-01-26 17:45:27 +0900

    modify

--- functions_db.php
+++ functions_db.php
@@ -67,9 +67,9 @@
 	 echo "<th>".$column2."</th>";
 	 echo '</tr>';
 
-echo "SELECT $column1,$column2 FROM $table";
+echo "SELECT ".$column1.",".$column2." FROM $table";
 
-	 $table_data = mysql_query("SELECT $column1,$column2 FROM $table");
+	 $table_data = mysql_query("SELECT ".$column1.",".$column2." FROM $table");
 	 while ($data = mysql_fetch_array($table_data, MYSQL_ASSOC)) {
 	     $datas[] = $data;
 	 }
